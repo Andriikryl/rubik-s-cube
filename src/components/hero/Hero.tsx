31d6a356433c90bb1709d71169faca55cfcb7de1
Andriikryl <andreyk557@gmail.com>
2023-11-09 12:10:20 +0200

    add updateAction for shiftOperation

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,29 +1,53 @@
-import React from "react";
-import { ShiftOperation } from "../types/types";
+"use client";
+import React, { useReducer } from "react";
+import { CubeData, Cycle, ShiftOperation } from "../types/types";
 import { Container } from "../container/Container";
 import { createCube, shift } from "../helpers/helpers";
 import Cube from "../cube/Cube";
 import style from "./style.module.css";
 
+interface UpdateAction {
+  operation: ShiftOperation;
+  cycle: Cycle;
+}
+
 export default function Hero() {
-  const cube = createCube();
-  const cycel = [2, 3, 4, 5] as const;
+  const [cube, dispatch] = useReducer(
+    (cube: CubeData, { operation, cycle }: UpdateAction) => {
+      return shift(cube, cycle, operation);
+    },
+    createCube()
+  );
 
-  const operation: ShiftOperation = [
-    [1, 1, 1],
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
+  // const cycel = [2, 3, 4, 5] as const;
 
-  const cube2 = shift(cube, cycel, operation);
+  const cycels: Record<string, Cycle> = {
+    HORIZONTAL: [2, 3, 4, 5],
+  };
+
+  const operations: Record<string, ShiftOperation> = {
+    U1: [
+      [1, 1, 1],
+      [0, 0, 0],
+      [0, 0, 0],
+    ],
+  };
+
+  // const cube2 = shift(cube, cycel, operation);
 
   return (
     <section>
       <Container>
         <div className={style.flex__group}>
           <Cube cube={cube} />
-          <Cube cube={cube2} />
         </div>
+        <button
+          onClick={() =>
+            dispatch({ operation: operations.U1, cycle: cycels.HORIZONTAL })
+          }
+        >
+          left
+        </button>
       </Container>
     </section>
   );
